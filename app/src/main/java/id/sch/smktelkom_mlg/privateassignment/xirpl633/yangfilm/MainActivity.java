7a9bb7fcb7d76fdbf6ed1e094788553349d26408
salsapr <salsabilaputrirayieningtyas@gmail.com>
2017-05-14 22:46:23 +0700

    Desain(2)

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl633/yangfilm/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl633/yangfilm/MainActivity.java
@@ -4,9 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -15,7 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import id.sch.smktelkom_mlg.privateassignment.xirpl633.yangfilm.adapter.ComingSoonAdapter;
 import id.sch.smktelkom_mlg.privateassignment.xirpl633.yangfilm.adapter.NowPlayingAdapter;
@@ -47,15 +44,6 @@ public class MainActivity extends AppCompatActivity
         });
 
         thread.start();
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
